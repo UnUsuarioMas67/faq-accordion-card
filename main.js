e901634930293885a2034ab36474d6b3715847a8
Bryan Sanchez <bsn9704@gmail.com>
2023-09-28 23:48:14 -0400

    refactored code into openAccordion function

--- main.js
+++ main.js
@@ -5,10 +5,15 @@ for (i = 0; i < questionContainers.length; i++) {
 	let question = element.getElementsByClassName("question")[0];
 
 	question.addEventListener("click", () => {
-		if (!element.classList.contains("active")) {
-			element.classList.add("active");
-		} else {
-			element.classList.remove("active");
-		}
+		openAccordion(element)
 	});
+}
+
+function openAccordion(accordionElement) {
+	if (accordionElement.classList.contains("active")) {
+		accordionElement.classList.remove("active")
+		return
+	}
+
+	accordionElement.classList.add("active")
 }
